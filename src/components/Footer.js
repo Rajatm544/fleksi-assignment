865a642d4108fddf424d137b09f401479145de0f
Rajatm544 <rajatm544@gmail.com>
2021-03-01 00:12:21 +0530

    add a footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="icons">
-                <i class="lni lni-twitter-original"></i>
-                <i class="lni lni-facebook-original"></i>
-                <i class="lni lni-instagram-original"></i>
-                <i class="lni lni-behance-original"></i>
+                <i className="lni lni-twitter-original"></i>
+                <i className="lni lni-facebook-original"></i>
+                <i className="lni lni-instagram-original"></i>
+                <i className="lni lni-behance-original"></i>
             </div>
             <p className="copyright">&#169; 2021 expice Ltd</p>
         </footer>
