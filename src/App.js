9dcc2cf9749819a859c349e5b3f59f6435d7bd01
Rajatm544 <rajatm544@gmail.com>
2021-02-28 23:49:16 +0530

    render all components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 // import "./App.css";
 import Landing from "./components/Landing";
+import About from "./components/About";
+import PreBook from "./components/PreBook";
+import ExploreMenu from "./components/ExploreMenu";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+import "./App.css";
 function App() {
     return (
         <div className="App">
             <Landing />
+            <About />
+            <PreBook />
+            <ExploreMenu />
+            <Contact />
+            <Footer />
         </div>
     );
 }
