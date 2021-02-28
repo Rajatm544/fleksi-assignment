9549bb5c0c6f1a51ff54b6e67d1fc3eb548691b4
Rajatm544 <rajatm544@gmail.com>
2021-02-28 09:21:19 +0530

    finish work on the landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+// import "./App.css";
+import Landing from "./components/Landing";
 function App() {
     return (
         <div className="App">
-            <header className="App-header"></header>
+            <Landing />
         </div>
     );
 }
---
+++ src/components/Landing.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import "../stylesheets/landing.css";
+import landingImg from "../images/food-top.png";
+import dots from "../images/dots.png";
+
+const Landing = () => {
+    const [state, setState] = useState({
+        logo: "expice",
+
+        name: "Restaurant Nidda",
+    });
+
+    return (
+        <main className="landing">
+            <nav className="navbar">
+                <strong className="nav-brand">{state.logo}</strong>
+                <div className="hamburger-icon">
+                    <div className="stick1"></div>
+                    <div className="stick1"></div>
+                    <div className="stick2"></div>
+                </div>
+            </nav>
+            <section className="background">
+                <div className="left-section">
+                    <p className="food-text">Food</p>
+                    <p className="restaurant-name">{state.name}</p>
+                    <button className="order-btn">Order Now</button>
+                </div>
+                <div className="right-section">
+                    <i className="lni lni-arrow-left-circle"></i>
+                    <i className="lni lni-arrow-right-circle"></i>
+                </div>
+                <div className="dots-bg">
+                    <img className="dots" src={dots} alt="dots" />
+                </div>
+
+                <img
+                    className="landing-food-img"
+                    src={landingImg}
+                    alt="food top view"
+                />
+            </section>
+        </main>
+    );
+};
+
+export default Landing;
